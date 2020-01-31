136b7201004934f7c34189f5f4ff0defdbf7d0f4
TPolanco <tp2523@columbia.edu>
2020-01-30 19:48:29 -0500

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,7 @@
+#Note that this is a template file for the credentials to access your own server (SQL Server)
+#Please fill in the dict below with your own server's credentials.
+
+cred = dict(Driver='{}',
+            Server='',
+            Database='',
+            Trusted_Connection='')
